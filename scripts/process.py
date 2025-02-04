e13dbac877979b6a2b296df21ae44a9b2b4dc558
gradedSystem <homesiknessgerald@gmail.com>
2025-02-04 15:56:25 +0800

    [modify][m] Script update

--- scripts/process.py
+++ scripts/process.py
@@ -1,58 +1,54 @@
-import urllib2
+import urllib.request
 import zipfile
-from tabulator import Stream
-from csv import writer
+import csv
+import os
+from frictionless import Resource
 
-source_url = 'https://www.bis.org/statistics/full_bis_selected_pp_csv.zip'
-
+source_url = 'https://www.bis.org/statistics/full_spp_csv.zip'
 
 def download_source():
     """ Downloads and unzip the source csv file
     :return: filename """
-    u = urllib2.urlopen(source_url)
-    with open('archive/source.zip', 'wb') as f:
+    os.makedirs('archive', exist_ok=True)
+    
+    with urllib.request.urlopen(source_url) as u, open('archive/source.zip', 'wb') as f:
         f.write(u.read())
-
-    with zipfile.ZipFile('archive/source.zip') as zfile:
+    
+    with zipfile.ZipFile('archive/source.zip', 'r') as zfile:
         zfile.extractall('archive/')
         return 'archive/' + zfile.namelist()[0]
 
-
 def parse_date(date):
-    """ transforms date from quartals '1999-Q1' to usual '1999-03-31' """
+    """ Transforms date from quartals '1999-Q1' to usual '1999-03-31' """
     return date.replace('Q1', '03-31').replace('Q2', '06-30').replace('Q3', '09-30').replace('Q4', '12-31')
-
 
 def parse(filename):
     """ Read csv file and parse it into 4 different csv files with unpivoted data."""
-    input_table = Stream(filename).open().read()
-    headers = input_table.pop(0)
-    dates = headers[9:]
+    resource = Resource(filename)
+    data = resource.read_rows()
+    headers = resource.header
+    dates = headers[12:]
 
+    os.makedirs('data', exist_ok=True)
+    
     output = {
-        # "description": "Nominal Index, 2010 = 100",
-        "nominal_index": writer(open('data/nominal_index.csv', 'wb')),
-        # "description": "Nominal Year-on-year changes, in per cent",
-        "nominal_year": writer(open('data/nominal_year.csv', 'wb')),
-        # "description": "Real Index, 2010 = 100",
-        "real_index": writer(open('data/real_index.csv', 'wb')),
-        # "description": "Real Year-on-year changes, in per cent",
-        "real_year": writer(open('data/real_year.csv', 'wb'))
+        "nominal_index": csv.writer(open('data/nominal_index.csv', 'w', newline='')),
+        "nominal_year": csv.writer(open('data/nominal_year.csv', 'w', newline='')),
+        "real_index": csv.writer(open('data/real_index.csv', 'w', newline='')),
+        "real_year": csv.writer(open('data/real_year.csv', 'w', newline=''))
     }
-
-    # headers
+    
     for csv_file in output.values():
         csv_file.writerow(['date', 'country_code', 'country', 'price'])
-
-    # processing
+    
     for date in dates:
-        for row in input_table:
-            country_code = row[2]
-            country = row[3]
-            value = row[5]
-            unit = row[7]
-            price = row[headers.index(date)]
-
+        for row in data:
+            country_code = row['REF_AREA']
+            country = row['Reference area']
+            value = row['Value']
+            unit = row['Unit of measure']
+            price = row[date]
+            
             category = ''
             if 'Nominal' in value and 'Index' in unit:
                 category = 'nominal_index'
@@ -62,19 +58,22 @@ def parse(filename):
                 category = 'real_index'
             elif 'Real' in value and 'Year' in unit:
                 category = 'real_year'
-            output[category].writerow([parse_date(date), country_code, country, price])
-
+            
+            if category:
+                output[category].writerow([parse_date(date), country_code, country, price])
 
 def extract_csv_structure():
-    """helper function"""
-    from datapackage import Package
+    """Helper function"""
+    from frictionless import Package
     import json
+    
     with open('structure.txt', 'w') as file:
         file.write(
             json.dumps(
-                Package().infer('data/*.csv'),
+                Package('data').to_descriptor(),
                 indent=2,
-                sort_keys=True)
+                sort_keys=True
+            )
         )
 
 if __name__ == "__main__":
